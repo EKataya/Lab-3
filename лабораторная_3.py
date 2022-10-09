da7701a925ca63c7ef22b1d3f5e18da0aa56902f
EKataya <91426628+EKataya@users.noreply.github.com>
2022-10-09 21:48:50 +0500

    Добавили комментарии

--- лабораторная_3.py
+++ лабораторная_3.py
@@ -1,20 +1,20 @@
 print('Шифр Цезаря')
 alphabet =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-step=int(input('Шаг для шифрования:'))%26
-line=input('Строка для шифрования на английском языке:').upper()
-result=''
+#зашифровка сообщения
+step=int(input('Шаг для шифрования:'))%26 #создаем переменную с шагом шифровки
+line=input('Строка для шифрования на английском языке:').upper() #создаем переменную для сообщения
+result='' 
   
 for i in line:
-    m=alphabet.find(i)
-    n=m+step
+    m=alphabet.find(i) #находим место символа в алфавите
+    n=m+step #сдвигаем символ
     if i in alphabet:
-        result+=alphabet[n]
+        result+=alphabet[n] 
     else:
-        resuit += i
+        resuit += i #задаем значение в результат
 print('Зашифрованное слово: ',result)
 
-
+#расшифровка сообщения
 step=int(input('Шаг для шифрования:'))%26
 line = input("Строка для расшифровки на английском языке: ").upper()
 result = ''
